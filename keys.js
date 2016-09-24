3ed69c2b3b535093f8e775405440f04bc089aaa3
Mary Miller <marymiller1954@gmail.com>
2016-09-24 17:49:28 -0400

    working

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeyes = {
+
+consumer_key:        'GmX5IxBOhNhxEZhTkqOoNe6WR',
+
+consumer_secret:     'OGOnXTtRHX09eIirbYnq9k6GvYJEokbz5E6liLCjbXNDhog2qf',
+
+access_token_key:      	'148152554-Ly1P6Gle0h08fepi765Bxs9dCLmv4b98LeA5f3Dy',
+
+access_token_secret: 'oyAD1COGK29PhyPkso1eiAnkrdWB3eKgsmqFAHZSqRSmR'
+
+}
